e2cdf3fe92eeac4d08f1be742d989b9e3eb9366f
TakayukiTooyama <t.tooyama107@gmail.com>
2021-01-23 17:08:22 +0900

    PWA - Lighthouse最適化

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,18 +18,14 @@ class MyDocument extends Document {
     return (
       <Html lang="ja-JP">
         <Head>
-          <link
-            rel="apple-touch-icon"
-            type="image/png"
-            href="/apple-touch-icon-180x180.png"
-          />
-          <link rel="icon" type="image/png" href="/icon-192x192.png" />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/icon-32x32.png"
-          />
+          <meta name="application-name" content="NUHW TRACK" />
+          <meta name="theme-color" content="#000" />
+          <meta name="description" content="this is NUHW TRACK" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon-180x180.png" />
+          <link rel="icon" sizes="32x32" href="/icon-32x32.png" />
+          <link rel="icon" sizes="192x192" href="/icon-192x192.png" />
+          <link rel="icon" sizes="512x512" href="/icon-512x512.png" />
+          <link rel="icon" href="/favicon.ico" />
           <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
