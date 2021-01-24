f14f303774c9b72eef2aea0a8f04b7160e02156b
TakayukiTooyama <t.tooyama107@gmail.com>
2021-01-25 00:14:11 +0900

    inputの挙動テスト

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@ import type { NextPage } from 'next';
 import React from 'react';
 
 import { Home, Layout } from '../components/templates';
+import Test from './test';
 
 const HomePage: NextPage = () => (
   <Layout title="Home">
     <Home />
+    <Test />
   </Layout>
 );
 
